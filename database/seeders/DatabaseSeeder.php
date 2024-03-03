a1efa8194fb7fd0af0df9d7ff86f8448ea7bce69
houcinch <houcincharqy@gmail.com>
2024-03-03 17:36:56 +0100

    database migrations & seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Houcin ch',
+            'email' => 'houcinch@email.com',
+            'password' => '12345678'
+        ]);
     }
 }
